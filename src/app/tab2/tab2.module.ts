82411f838c28cb5ad8f067dfd8f0a49ecf469b5c
Guttz <gustavopguto@gmail.com>
2019-05-19 23:21:27 +0200

    Setting up config and experiment

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -1,9 +1,10 @@
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, Platform } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { EstimoteBeacons } from '@ionic-native/estimote-beacons/ngx';
 
 @NgModule({
   imports: [
@@ -12,6 +13,12 @@ import { Tab2Page } from './tab2.page';
     FormsModule,
     RouterModule.forChild([{ path: '', component: Tab2Page }])
   ],
-  declarations: [Tab2Page]
+  declarations: [Tab2Page],
+  providers: [
+    EstimoteBeacons
+  ]
 })
-export class Tab2PageModule {}
+
+export class Tab2PageModule {
+
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { Component,  } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import { EstimoteBeacons, EstimoteBeaconRegion } from '@ionic-native/estimote-beacons/ngx';
+
+let cordova: any;
+let evothings: any;
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {}
+export class Tab2Page {
+
+  constructor(private platform: Platform, private eb: EstimoteBeacons) {
+  }
+
+
+
+  startScanningForBeacons() {
+  // Request permission to use location on iOS
+
+  var region: EstimoteBeaconRegion = {uuid: '636f3f8f-6491-4bee-95f7-d8cc64a863b5', major: 0, minor: 0};
+
+  this.eb.startRangingBeaconsInRegion(region).subscribe( (data) => {
+    console.log( JSON.stringify(data));
+  });
+
+/*   // Start ranging
+this.eb.startMonitoringForRegion(region, true)
+.sub(
+  () => console.log('received the request to monitoring'),
+  error => console.error('failed to begin monitoring: ', error)
+);
+// Stop ranging
+this.eb.stopMonitoringForRegion() */
+
+}
+
+
+  startScanningForBeacons2() {
+    this.platform.ready().then( () => {
+      evothings.eddystone.startScan( (beaconData) => {
+        console.log(beaconData);
+      }, error => { console.log(error)});
+    });
+  }
+
+}
